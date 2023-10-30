800fcad99d76b95de1570c831fdbb2359e061552
KumJungMin <rmawjdals@naver.com>
2023-10-30 21:32:35 +0900

    2. flocking rule을 사용해 중복 코드를 추상화하기

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import TextField from "./component/TextField";
 import Form from "./component/Form";
 import CheckboxField from "./component/CheckboxField";
 
+// flocking rule이란?
+// 아래 3단계를 걸쳐, 중복 코드를 제거하고, 코드를 재사용 가능하게 만드는 것을 말합니다.
+// 1.코드들은 비교하여 가장 비슷한 부분을 찾는다.
+// 2. 비슷한 코드 사이에 다른 것을 찾는다.
+// 3. 차이가 발생하는 부분을 제거하여 하나의 일반화된 코드로 만든다.
+
 export interface Info {
   name: string;
   password: string;
@@ -23,8 +29,16 @@ function App() {
   };
 
   return (
+    // 1. 두 필드의 공통점은 value, setValue 동작을 한다는 점
+    // 2. 다른 점은 설정하는 value가 다르다는 점
+    // 3. 각 컴포넌트 내부에서 추상화된 props를 받도록 수정하면 됨
     <Form onSubmit={onSubmit}>
-      <TextField source="name" value={info} setValue={setInfo} label="이름" />
+      <TextField
+        source="name"
+        value={info}
+        setValue={setInfo}
+        label="이름"
+      />
       <CheckboxField
         value={info}
         source="confirm"
--- src/component/TextField.tsx
+++ src/component/TextField.tsx
@@ -9,6 +9,7 @@ const TextField: React.FC<{
 }> = ({ value, setValue, label, source }) => {
   return (
     <>
+      {/* 재사용성을 높이기 위해 추상화된 값을 props로 넘긴다 */}
       {label}
       <input
         onChange={(e) => setValue({ ...value, [source]: e.target.value })}
